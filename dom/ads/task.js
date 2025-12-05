4238f80f4aa532c8dcd8e2fb32c5611156bf0062
DianaSha2711 <dbravo2711@gmail.com>
2025-12-05 11:52:40 +0300

    Читалка 1

---
+++ dom/ads/task.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const fontSizeControl = document.querySelector('.book__control_font-size');
+    const bookElement = document.getElementById('book');
+    
+
+    if (!fontSizeControl || !bookElement) return;
+    
+    fontSizeControl.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        const target = event.target.closest('.font-size');
+        if (!target) return;
+        
+        document.querySelectorAll('.font-size_active').forEach(el => {
+            el.classList.remove('font-size_active');
+        });
+        
+        target.classList.add('font-size_active');
+        
+        
+        const selectedSize = target.dataset.size;
+        
+     
+        bookElement.classList.remove('book_fs-small', 'book_fs-big');
+        
+        
+        if (selectedSize === 'small') {
+            bookElement.classList.add('book_fs-small');
+        } else if (selectedSize === 'big') {
+            bookElement.classList.add('book_fs-big');
+        }
+      
+    });
+});
